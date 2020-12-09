4e9e0998931fc84df48568f69681c8fb2e3d18f2
unknown <sonypulickeel@gmail.com>
2020-12-09 09:51:08 +0530

    auto complete box

--- src/components/HorizontalLayout/Header.js
+++ src/components/HorizontalLayout/Header.js
@@ -17,7 +17,9 @@ import logo from "../../assets/images/annalogocolor.png";
 import logoLight from "../../assets/images/annalogocolor.png";
 import logoLightSvg from "../../assets/images/annalogocolor.png";
 import logoDark from "../../assets/images/annalogocolor.png";
-
+import Autocomplete from "react-autocomplete"
+import { BaseUrl } from "../../config/BaseUrl";
+import { apiGet } from "../../config/apiConfig";
 // Redux Store
 import { toggleRightSidebar } from "../../store/actions";
 
@@ -27,14 +29,25 @@ import { withNamespaces } from "react-i18next";
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = { isSearch: false };
+    this.state = { isSearch: false,value:"" };
     this.toggleSearch = this.toggleSearch.bind(this);
   }
 
   toggleSearch = () => {
     this.setState({ isSearch: !this.state.isSearch });
   };
-
+  handleChange(e){
+   
+    this.setState({value:e.target.value})
+    const Url = BaseUrl.apiUrl.baseUrl + "api/public/search/"+e.target.value;
+    let resp = apiGet(Url);
+    resp.then((resp)=> {
+      console.log('oooooooooooooooooo',resp)
+    })
+  }
+  handleSelect(val){
+    // console.log('bbbbbbbbbbbbbbbbbbb',val)
+  }
   render() {
     var lat = "7.568443,2.485789";
     var long = "";
@@ -77,7 +90,7 @@ class Header extends Component {
               >
                 <i className="fa fa-fw fa-bars"></i>
               </button>
-              <form className="app-search d-none d-lg-block">
+              {/* <form className="app-search d-none d-lg-block">
                 <div className="position-relative">
                   <input
                     type="text"
@@ -86,7 +99,23 @@ class Header extends Component {
                   />
                   <span className="bx bx-search-alt"></span>
                 </div>
-              </form>
+              </form> */}
+              <Autocomplete
+              getItemValue={(item) => item.label}
+              items={[
+                { label: 'apple' },
+                { label: 'banana' },
+                { label: 'pear' }
+                ]}
+                renderItem={(item, isHighlighted) =>
+                <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                  {item.label}
+              </div>
+  }
+  value={this.state.value}
+  onChange={(e) => this.handleChange(e)}
+  onSelect={(val) => this.handleSelect(val)}
+/>
               <div className="headerlocation">
                 Delivering to
                 <span className="deli">{lat}</span>
